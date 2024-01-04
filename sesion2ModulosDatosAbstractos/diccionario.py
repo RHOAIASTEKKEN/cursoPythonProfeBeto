6479cc29ba41b53d789c15ca61dc01b2d26cfa4e
RHOAIASTEKKEN <84831203+RHOAIASTEKKEN@users.noreply.github.com>
2024-01-03 18:15:11 -0600

    termino de sesion

---
+++ sesion2ModulosDatosAbstractos/diccionario.py
@@ -0,0 +1,10 @@
+diccioanrioTemas={'JAVA':'joyanes','c':'cepallos','angular':'grossmann','c++':'deitel','php':'diaz'}
+
+print(diccioanrioTemas)
+
+print("las claves son: \n", diccioanrioTemas.keys())
+
+print("valores", diccioanrioTemas.values())
+
+print("el valor de la clave es:",diccioanrioTemas['JAVA'])
+print("la clave del valor es:",diccioanrioTemas['c'])
