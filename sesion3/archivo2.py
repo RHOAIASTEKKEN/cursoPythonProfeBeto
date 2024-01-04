c250b1a679ec381a079b0aa384b067188da48317
RHOAIASTEKKEN <84831203+RHOAIASTEKKEN@users.noreply.github.com>
2024-01-04 17:44:13 -0600

    semi final sesion 3

--- sesion3/archivo2.py
+++ sesion3/archivo2.py
@@ -1,3 +1,24 @@
 import os
-
 import sys
+
+dirActual = os.getcwd()
+
+print("path es: ", dirActual)
+
+passwords = set()
+
+try:
+    # Abre el archivo 'archivo.txt' en modo lectura
+    with open('archivo.txt', 'r') as fichero:
+        for line in fichero:
+            passwords.add(line.strip())
+except FileNotFoundError:
+    print("El archivo no se encuentra.")
+    sys.exit()
+
+password_ingresado = input("Ingresa el password a buscar\n")
+
+if password_ingresado in passwords:
+    print("Password encontrado y es:", password_ingresado)
+else:
+    print("No se encontró el password:", password_ingresado)
---
+++ sesion3/import2.py
@@ -0,0 +1,15 @@
+import urllib.request
+
+url = 'https://es.wikipedia.org/wiki/Wikipedia:Portada'
+
+# Realiza la solicitud
+dataWeb = urllib.request.urlopen(url)
+
+# Lee el contenido de la respuesta
+content = dataWeb.read()
+
+# Guarda el contenido en un archivo
+with open('import2.txt', 'wb') as archivo:
+    archivo.write(content)
+
+print(f'El contenido ha sido guardado en "import2.txt"')
